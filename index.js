4958fe97aab4d0109148d5ddf222d85cffa94102
Michael <michael.garstka@gmail.com>
2017-06-30 22:42:18 +0200

    Working timestamp microservice

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
- var app = express();
-
+var u = require("url");
+var moment = require("moment");
+var app = express();
 
 app.use(express.static(__dirname));
 
@@ -8,6 +9,26 @@ app.use(express.static(__dirname));
  app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
  });
+
+
+/* serves all api requests */
+app.get("/:timestamp", function(req, res) {
+    var timestamp = moment(req.params.timestamp, 'MMMM DD, YYYY', true);
+    var content;
+    // if timestamp is not human readable check if it's in the unix format
+    if (!timestamp.isValid()){
+        timestamp = moment.unix(req.params.timestamp);
+    }
+
+    if (!timestamp.isValid()){
+        content = {unix: null, natural: null};
+    } else{
+        content = {unix: timestamp.format("X"),natural: timestamp.format("MMMM DD, YYYY")};
+    }
+
+    res.json(content);
+ });
+
 
 var port = process.env.PORT || 5000;
  app.listen(port, function() {
